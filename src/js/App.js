77e7d72eb86bab8361a6c57b739e9d016e50240d
Konstantin Kalinin <kalinin.konst.official@gmail.com>
2022-10-07 14:28:01 +0300

    Добавил адаптив на страницу меню и на сайт с играми

--- src/js/App.js
+++ src/js/App.js
@@ -1,8 +1,3 @@
-window.addEventListener('scroll', () => {
-    let scroll = document.querySelector('.footer__button-up-image');
-    scroll.classList.toggle("active", window.scrollY > 200)
-})
-
 function scrollToPos(position) {
     window.scrollTo({top: position, behavior: "smooth"})
 }
